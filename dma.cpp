1c483cefe9d42fcd13870061523b7d81bc796317
Gary He <jiabaohe0@gmail.com>
2025-01-23 09:18:58 -0500

    Add files via upload

---
+++ dma.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "dma.h"
+using namespace std;
+namespace seneca {
+
+	void reverse() {
+		int count;
+		std::cout << "Enter the number of double value: ";
+		std::cin >> count;
+
+		double* arr = new double[count];
+		for (int i = 0; i < count; i++) {
+			std::cout << i + 1 << "> ";
+			std::cin >> arr[i];
+		}
+
+		for (int i = count - 1; i >= 0; --i) {
+			std::cout << arr[i] << std::endl;
+		}
+
+		//delete[] arr;
+	}
+
+    Contact* getContact() {
+        Contact* contact = new Contact;
+        std::cout << "Name: ";
+        std::cin.ignore(); // Clear newline
+        std::cin.getline(contact->m_name, 31);
+
+        std::cout << "Last name: ";
+        std::cin.getline(contact->m_lastname, 31);
+
+        std::cout << "Phone number: ";
+        std::cin >> contact->m_phoneNumber;
+
+        return contact;
+    }
+
+    // Display contact details
+    void display(const Contact& contact) {
+        std::cout << contact.m_name << " " << contact.m_lastname
+            << ", +" << contact.m_phoneNumber << std::endl;
+    }
+
+    // Set contact details to empty values
+    void setEmpty(Contact& contact) {
+        contact.m_name[0] = '\0';
+        contact.m_lastname[0] = '\0';
+        contact.m_phoneNumber = 0;
+    }
+
+    // Deallocate a dynamically allocated Contact
+    void deallocate(Contact* contact) {
+        delete contact;
+    }
+
+
+
+
+}
---
+++ input.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "input.h"
+using namespace std;
+namespace seneca {
+   int getInt() {
+      int num = 0;
+      bool done = false;
+      do {
+         // extract an integer from the console and put it in num
+         cin >> num;
+         // if this action fails (the integer is unreadable)
+         if (cin.fail()) {
+            cout << "Bad integer entry, please try again:\n> ";
+            // clear the failure status to activate cin again
+            cin.clear();
+         }
+         else {
+            done = true;
+         }
+         // flush the keyboard buffer up to 1000 characters or '\n', whichever comes first
+         cin.ignore(1000, '\n');
+      } while (!done);
+      return num;
+   }
+
+   int getInt(int min, int max) {
+       int number;
+       while (true) {
+           number = getInt();
+           if (number >= min && number <= max) break;
+           std::cout << "Invalid value, [" << min << "<=ENTRY<=" << max << "]: ";
+       }
+       return number;
+   }
+
+   bool getInt(int& num, int min, int max) {
+       num = getInt();
+       if (num >= min && num <= max) {
+           return true;
+       }
+       else {
+           std::cout << "Only enter values between " << min << " and " << max << ", inclusive: ";
+           return false;
+       }
+   }
+  
+}
+

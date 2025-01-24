859a72a5783d6f2c81acced78b48f97deaf459b6
Gary He <jiabaohe0@gmail.com>
2025-01-24 12:13:00 -0500

    Update dma.cpp

--- dma.cpp
+++ dma.cpp
@@ -5,7 +5,7 @@ namespace seneca {
 
 	void reverse() {
 		int count;
-		std::cout << "Enter the number of double values: ";
+		std::cout << "Enter the number of double values:\n>";
 		std::cin >> count;
 
 		double* arr = new double[count];
